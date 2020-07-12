95d63fb721044f6eb0639d4425438526d60c8163
evunexx <s.weist@me.com>
2020-07-12 21:28:07 +0200

    Add forms

--- login.py
+++ login.py
@@ -2,40 +2,47 @@ from flask import Flask, redirect, url_for, request, render_template, session, e
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField
 from wtforms.validators import DataRequired, Length
-from forms import LoginForm
+from forms import LoginForm, RegisterForm
 import time
 
 app = Flask(__name__)
 app.secret_key = "foobar"
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
     form = LoginForm()
     if 'username' in session:
         username = session['username']
-        return render_template('dashboard.html', user = username)
+        return render_template('dashboard.html', user=username)
     if form.validate_on_submit():
         session['username'] = form.username.data
-        user = form.username.data
-        return redirect(url_for('dashboard', user=user))
-    return render_template('login.html', form=form)
+        return redirect(url_for('dashboard'))
+
+    return render_template('login.html', form = form)
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
     
-@app.route('/registration')
-def registration():
-    return 'pass'
+@app.route('/register')
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        session['username'] = form.username.data
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 
 @app.route('/dashboard', methods = ['POST', 'GET'])
-def dashboard(user):
+def dashboard():
+    user = session['username']
     #form = LoginForm()
     #if request.method == 'POST':
         #session['username'] = form.username.data
         #user = form.username.data
-        return render_template('dashboard.html', user = user)
+    return render_template('dashboard.html', user = user)
     
 if __name__ == '__main__':
    app.run(debug = True)   
--- templates/forms.py
+++ templates/forms.py
@@ -1,4 +1,5 @@
-from flask_wtf import FlaskForm
+from flask.ext.wtf import Form 
+#from flask_wtf import FlaskForm
 from wtforms import (
                     StringField, 
                     TextField, 
@@ -6,7 +7,9 @@ from wtforms import (
                     PasswordField)
 from wtforms.validators import (
                                 DataRequired, 
-                                Length)
+                                Length,
+                                Email,
+                                EqualTo)
 
 class LoginForm(FlaskForm):
 
@@ -15,5 +18,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Name', [
         DataRequired(message="Please enter a password")])
     submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+
+    username = StringField('Username', 
+                    validators= [DataRequired(),
+                                Length(min=2 max=20)])
+    email = StringField('Email',
+                    validators= [DataRequired(),
+                                Email()])
+    password = PasswordField('Passwort',
+                    validators= [DataRequired()])
+    confirm_password = PasswordField('Passwort bestätigen',
+                    validators= [DataRequired(), 
+                                EqualTo('password')])
+    submit = SubmitField ('Abschicken')
+    
 
 
